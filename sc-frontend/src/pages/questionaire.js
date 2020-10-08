8507129563df0fcf47a9eda25a39fce6239b4367
Riju19 <19.riju@gmail.com>
2020-10-08 12:08:23 +0530

    migrate frontend code from zoey's repo to here

---
+++ sc-frontend/src/pages/questionaire.js
@@ -0,0 +1,13 @@
+//questionaire page
+import React, { Component } from "react";
+
+export default class Questionaire extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>questionaire</div>;
+  }
+}
---
+++ sc-frontend/src/pages/resources.js
@@ -0,0 +1,8 @@
+//render selected resources
+import React from "react";
+
+export const Resources = () => {
+  return <div> resource page </div>;
+};
+
+export default Resources;
