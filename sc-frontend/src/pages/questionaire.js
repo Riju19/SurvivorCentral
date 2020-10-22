1d85f4bd41411ddd867bacfc98d0dbc9585abdd2
Gavi Schneider <gavischneider@gmail.com>
2020-10-22 16:20:48 +0300

    Added question data structure skeleton

--- sc-frontend/src/pages/questionaire.js
+++ sc-frontend/src/pages/questionaire.js
@@ -1,6 +1,17 @@
 //questionaire page
 import React, { Component } from "react";
 
+// First thing I'm doing is creating the array of objects. Each object in the array will have two Properties:
+// 1. A question property, holding the actual question text
+// 2. A response property, which will initially be empty
+
+let questions = [
+  {
+    question: "",
+    response: ""
+  }
+]
+
 export default class Questionaire extends Component {
   constructor(props) {
     super(props);
