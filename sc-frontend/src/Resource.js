0a8ae24b0631ccb3efe5ed13da91d936302f2203
Gavi Schneider <gavischneider@gmail.com>
2020-11-18 15:34:09 +0200

    Added resourcesDetails object

--- sc-frontend/src/Resource.js
+++ sc-frontend/src/Resource.js
@@ -2,14 +2,11 @@ import React from "react";
 
 function Resource(props) {
   return (
-    <>
-      // Render details
-      <ul>
-        {props.details.map((detail) => {
-          <li>detail</li>;
-        })}
-      </ul>
-    </>
+    <ul>
+      {props.details.map((detail) => {
+        <li>{detail}</li>;
+      })}
+    </ul>
   );
 }
 export default App;
--- sc-frontend/src/pages/resources.js
+++ sc-frontend/src/pages/resources.js
@@ -2,23 +2,55 @@
 import React from "react";
 import Resource from "../Resource";
 
-const resourcesDetails = {};
+const resourcesDetails = {
+  hivAidsresources: {},
+  dayOne: {},
+  sojournerHouse: {},
+  elizabaethBuffamChase: {},
+  riChildrenAdvocacyCenter: {},
+  nationalDomesticViolenceHotline: {},
+  nationalResourceCenterOnDomesticViolence: {},
+  officeForVictimsOfCrime: {},
+  leaAndCourtAdvocateInfo: {},
+  vineCrimeVictimAdvocate: {},
+  dcyf: {},
+  nationalCenterForVictimsOfCrime: {},
+  riCvcp: {},
+  bradleyHospitalAndTidesFamilyService: {},
+  familyServicesOfRiAndFamilyCommunityCarePartnership: {},
+  dvPartnersUnderRICADVAndCounselingResources: {},
+  TRO: {},
+  undocumentedInfo: {},
+  trafickingSurvivorResources: {},
+  legalCounselingRestitutionLink: {},
+  legalAssistanceResources: {},
+  victimbarLink: {},
+  volunteerLawyerProgram: {},
+  homelessnessResources: {},
+  helplineRI: {},
+  CAP: {},
+  DHS: {},
+  GLBTQDomesticViolenceProject: {},
+  GLAD: {},
+  youthPrideInc: {},
+  relay: {},
+  NSPL: {},
+};
 
 export const Resources = (props) => {
+  // I'm assuming right now that props.resources holds the resources object from the previos page
   return (
-    <>
+    <div>
       {props.resources.map((resource) => {
         // Render each resource if its value is 'true'
         {
           if (resource) {
-            <Resource details={}></Resource>;
+            return <Resource details={resourcesDetails["resource"]}></Resource>;
           }
         }
       })}
-    </>
+    </div>
   );
 };
 
 export default Resources;
-
-// Make resourcves data structure that holds links and numbers
