30d501c3cc93e14b53e9784a38666caf4bad4303
Gavi Schneider <gavischneider@gmail.com>
2020-12-30 11:12:25 +0200

    Added card component, changed resources page to functional component

--- sc-frontend/src/components/resource/Resource.js
+++ sc-frontend/src/components/resource/Resource.js
@@ -1,36 +1,59 @@
 import React from "react";
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      '& > * + *': {
-        marginLeft: theme.spacing(2),
-      },
-    },
-  }),
-);
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+
+// const useStyles = makeStyles((theme) =>
+//   createStyles({
+//     root: {
+//       '& > * + *': {
+//         marginLeft: theme.spacing(2),
+//       },
+//     },
+//   }),
+// );
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 const Resource = (props) => {
   // Map over props.details and print out each value
   const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <Typography className={classes.root}>
-        {Object.keys(props.details).map(function (keyName, keyIndex) {
-          // use keyName to get current key's name
-          // and props.details[keyName] to get its value
-          return (
-            <Typography>
-              {props.details[keyName]}
-            </Typography>
-        )})}
-      </Typography>
-    </div>
+    <Card className="classes.root">
+      <CardContent>
+        <div>
+          <h2>{props.name}</h2>
+          <Typography className={classes.root}>
+            {Object.keys(props.details).map(function (keyName, keyIndex) {
+              // use keyName to get current key's name
+              // and props.details[keyName] to get its value
+              return <Typography>{props.details[keyName]}</Typography>;
+            })}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
   );
-}
+};
 export default Resource;
--- sc-frontend/src/pages/resources.js
+++ sc-frontend/src/pages/resources.js
@@ -1,49 +1,72 @@
 //render selected resources
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Resource from "../components/resource/Resource";
 import { resourcesDetails } from "../constants/resource_details.js";
-import { resources } from "../constants/resources.js";
+import { resources as rsr } from "../constants/resources.js";
 import "../index.css";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default class Resources extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      resources: resources,
-    };
-  }
-  componentDidMount() {
-    if (this.props.location.props) {
-      console.log(this.props.location.resources);
-      this.setState(this.props.location.resources);
+const Resources = (props) => {
+  console.log(`PROPS: `);
+  console.log(props);
+  const [resources, setResources] = useState(rsr);
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(1),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+  const classes = useStyles();
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     resources: resources,
+  //   };
+  // }
+  // componentDidMount() {
+  //   if (this.props.location.props) {
+  //     console.log(this.props.location.resources);
+  //     this.setState(this.props.location.resources);
+  //   }
+  // }
+
+  useEffect(() => {
+    if (props.location.props) {
+      console.log(props.location.resources);
+      setResources(props.location.resources);
     }
-  }
+  }, [props.location.props]);
 
-  render() {
-    const resources = this.state.resources;
-    const resourceKeys = Object.keys(resources);
-    return (
-      <div>
-        <div className="header">
-          <h1>Resources</h1>
-        </div>
-        <Grid container spacing={1}>
+  const resourceKeys = Object.keys(resources);
+
+  return (
+    <div>
+      <div className="header">
+        <h1>Resources</h1>
+      </div>
+      <Grid item xs={4} container spacing={1}>
         {resources != null &&
           resourceKeys.map(function (keyName) {
             // use keyName to get current key's name
             // and resources[keyName] to get its value
             return resources[keyName] ? (
               <Grid container item xs={12} spacing={3}>
-              <Resource
-                name={keyName}
-                details={resourcesDetails[keyName]}
-              />
+                <Resource name={keyName} details={resourcesDetails[keyName]} />
               </Grid>
             ) : null;
           })}
-          </Grid>
-      </div>
-    );
-  }
-}
+      </Grid>
+    </div>
+  );
+};
+
+export default Resources;
